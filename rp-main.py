d589daed8b4c1678b28eeffbe172857a4c3f848a
Aaron-Li <1470409562@qq.com>
2023-03-23 13:28:23 +0800

    UPDATE

--- rp-main.py
+++ rp-main.py
@@ -3,8 +3,14 @@ import random
 
 st.title('random picture')
 
-pic_num = random.randint(1, 3)
-pic_url = ""
+pic_dir_num = random.randint(0, 1)
+if pic_dir_num == 0:
+    pic_num = random.randint(1, 1)
+    pic_url = "https://github.com/aaron-lii/random-pic/raw/main/pics/" + str(pic_num) + ".png"
+else:
+    pic_num = random.randint(1, 2)
+    pic_url = "https://github.com/aaron-lii/random-pic/raw/main/gifs/" + str(pic_num) + ".gif"
+
 
 st.image(pic_url)
 
